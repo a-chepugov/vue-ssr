da34237d09db2a2a9a3dc945d504ed16574a97bf
Алексей <oleksii.chepugov@ria.com>
2018-01-24 19:28:11 +0200

    fix recursiveComponentInit

--- vue/helpers/recursiveComponentInit.js
+++ vue/helpers/recursiveComponentInit.js
@@ -10,7 +10,7 @@ export default function (Component = {}, fnName, args) {
 		if (components instanceof Object) {
 			Object
 				.values(components)
-				.forEach((item) => recursive(item, fnName))
+				.forEach((item) => recursive(item, fnName, args))
 		}
 	}
 
