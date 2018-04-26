7b441de0f01d8ceb129e8aa20b53ae3705f8d0b2
a.chepugov <oleksii.chepugov@ria.com>
2018-04-26 14:46:32 +0300

    fix. production

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,10 @@ import dynamic from './dynamic';
 
 module.exports = function (app) {
 	dynamic(app);
-
+	require('./static')(app);
 	if (process.env.NODE_ENV === 'development') {
 		require('./webpack').default(app);
 	} else {
 		require('../controllers/vue').default(app);
 	}
-	require('./static')(app);
 };
